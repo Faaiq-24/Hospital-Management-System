d3d254d7680ece7c203dc3dcbbaa9ec6a44603b8
Muhammad Faaiq <faaiqzahid321@gmail.com>
2026-01-29 00:32:09 +0500

    Updated files

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -7,22 +8,70 @@ public class Main {
 
         Hospital hospital = new Hospital();
 
-    Doctor doctor1 = new Doctor("Dr Hafeez",123,40,"Pediatrician");
-    Doctor doctor2 = new Doctor("Dr ahmed",124, 35,"Therapist");
+        System.out.print("How many doctors do you want to add? ");
+        int dCount = sc.nextInt();
+        sc.nextLine(); // consume newline
 
-    Patient Patient1 = new Patient("Jawad",20,21,"Fever");
-    Patient Patient2 = new Patient("John",28, 18,"Headache");
+        for(int i = 0; i < dCount; i++) {
+            System.out.println("\nEnter details for Doctor " + (i + 1));
 
-//        hospital.addDoctor(doctor1);
-//        hospital.addDoctor(doctor2);
-//        hospital.addPatient(Patient1);
-//        hospital.addPatient(Patient2);
-//
-//        hospital.DisplayDoctors();
-//        hospital.DisplayPatients();
+            System.out.print("Doctor Name: ");
+            String name = sc.nextLine();
 
-        doctor1.displayinfo();
+            System.out.print("Doctor ID: ");
+            int id = sc.nextInt();
 
-        Patient1.displayinfo();
+            System.out.println("Doctor Age: ");
+            int age = sc.nextInt();
+
+            sc.nextLine();
+            System.out.print("Specialty: ");
+            String specialty = sc.nextLine();
+
+
+            Doctor d = new Doctor(name, id, age ,specialty);
+            hospital.addDoctor(d);
+
+        }
+
+
+        System.out.print("\nHow many patients do you want to add? ");
+        int pCount = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        for(int i = 0; i < pCount; i++) {
+            System.out.println("\nEnter details for Patient " + (i + 1));
+
+            System.out.print("Patient Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Patient Age: ");
+            int age = sc.nextInt();
+            sc.nextLine(); // consume newline
+
+            System.out.print("Patient ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Enter Patient Illness: ");
+            String illness = sc.nextLine();
+
+            Patient p = new Patient(name, age, id,illness);
+            hospital.addPatient(p);
+        }
+
+
+        hospital.DisplayDoctors();
+        hospital.DisplayPatients();
+
+        Doctor d1 = new Doctor("Dr john" ,1234,40,"Pediatrician" );
+        d1.displayinfo();
+        Patient p1 = new Patient("Yahya",4321,10,"Fever");
+        p1.displayinfo();
+
+
+
+
+
     }
 }
